f26d54e1e4eb982dfc863fab27c7e607a06680a8
Franke <henriquefranke31@gmail.com>
2026-01-23 23:10:51 -0300

    sg

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.status(200).send("API ONLINE 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5432;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta", PORT);
