2a2e9e504e6678ddcca698db57b5af6fe8262d69
Franke <henriquefranke31@gmail.com>
2026-01-23 21:52:33 -0300

    asg

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require("express");
-
-const mlAuthRoutes = require("./routes/mlAuth");
+import express from "express";
+import mlAuthRoutes from "./src/routes/mlAuth.js";
 
 const app = express();
 
 app.use(express.json());
 
-// rotas Mercado Livre
-app.use("/ml", mlAuthRoutes);
-
-// healthcheck Railway
+// healthcheck obrigatório
 app.get("/", (req, res) => {
   res.status(200).send("API ONLINE");
 });
 
+// Mercado Livre
+app.use("/ml", mlAuthRoutes);
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Server rodando na porta", PORT);
 });
